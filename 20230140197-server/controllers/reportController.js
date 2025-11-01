1491e646a5fe2cfe870aeefc0b45df666a51191d
jaddu17 <shafajaddu@gmail.com>
2025-11-01 15:30:44 +0700

    memperbaiki report controller

--- 20230140197-server/controllers/reportController.js
+++ 20230140197-server/controllers/reportController.js
@@ -3,25 +3,12 @@ const { Op } = require("sequelize");
 
 exports.getDailyReport = async (req, res) => {
   try {
-    const { nama, tanggal, tanggalMulai, tanggalSelesai } = req.query;
+    const { nama } = req.query;
     let options = { where: {} };
 
-    // Filter berdasarkan nama
     if (nama) {
-      options.where.nama = { [Op.like]: `%${nama}%` };
-    }
-
-    // Filter berdasarkan tanggal tertentu
-    if (tanggal) {
-      options.where.checkIn = {
-        [Op.between]: [`${tanggal} 00:00:00`, `${tanggal} 23:59:59`],
-      };
-    }
-
-    // Filter berdasarkan rentang tanggal
-    if (tanggalMulai && tanggalSelesai) {
-      options.where.checkIn = {
-        [Op.between]: [`${tanggalMulai} 00:00:00`, `${tanggalSelesai} 23:59:59`],
+      options.where.nama = {
+        [Op.like]: `%${nama}%`,
       };
     }
 
@@ -32,10 +19,9 @@ exports.getDailyReport = async (req, res) => {
       data: records,
     });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      message: "Gagal mengambil laporan presensi",
-      error: error.message,
-    });
+    res
+      .status(500)
+      .json({ message: "Gagal mengambil laporan", error: error.message });
   }
 };
+
